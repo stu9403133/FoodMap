9c3f471f6d2d870cda4b897601812895b7b70462
stu <stu382095@gmail.com>
2026-02-09 14:49:17 +0800

    Initial Commit

---
+++ FoodMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodMap
+//
+//  Created by 涂羽華 on 2026/2/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodMap/FoodMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMapApp.swift
+//  FoodMap
+//
+//  Created by 涂羽華 on 2026/2/9.
+//
+
+import SwiftUI
+
+@main
+struct FoodMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
